dc0a436550c9b3fb820c4dd2e5fa2f4d97ea1372
Pugsworth <kwvortax114@hotmail.com>
2017-04-13 02:27:46 +0000

    Added more.

---
+++ Youtube/youtube_remove_branded_box.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name        Remove Youtube's Shitty Autoplaying User Video
+// @namespace   pugsworth.wellington
+// @include     https://www.youtube.com/user/*
+// @include     https://youtube.com/user/*
+// @version     1
+// @grant       none
+// @run-at      document-end
+// ==/UserScript==
+
+function main(evt) {
+    var pageBoxes = document.getElementsByClassName("branded-page-box");
+
+    for (var i = 0; i < pageBoxes.length; i++) {
+        var pb = pageBoxes[i];
+
+        if (pb && pb.classList.contains("video-player-view-component")) {
+            pb.remove();
+        }
+    }
+}
+
+window.addEventListener("DOMContentLoaded", main);
+
